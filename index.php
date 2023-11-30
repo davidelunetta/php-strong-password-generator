38be826c26da4cb86a67834cd2bcc87ffe79f68f
davidelunetta <davidelunetta.27@gmail.com>
2023-11-30 14:26:26 +0100

    created structure

--- index.php
+++ index.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
     <title>PHP-password-generator</title>
 </head>
 <body>
-    
+    <div class="mt-5 container d-flex flex-column align-items-center">
+    <h1>Strong Password Generator</h1>
+    <h2 class="text-light">Genera una password sicura</h2>
+    <form action="index.php" method="GET">
+        <label for="passwordLength">Inserisci la lunghezza della password:</label>
+        <input type="number" id="passwordLength" name="passwordLength" min="1" max="25" required>
+        <button type="submit">Genera Password</button>
+        <button type="reset">Reset</button>
+    </form>
+    </div>
+   
+
 </body>
 </html>
